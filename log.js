6422f662a115d85863c835a7771fc6130808a68f
Lata Amenu <amelatbay@gmail.com>
2026-06-22 11:32:15 +0300

    log file size limited

--- log.js
+++ log.js
@@ -1,9 +1,15 @@
 import winston from "winston";
+import "winston-daily-rotate-file";
 
 const logger = winston.createLogger({
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: "sync.log" }),
+        new winston.transports.DailyRotateFile({
+            filename: "logs/sync-%DATE%.log",
+            datePattern: "YYYY-MM-DD",
+            maxSize: "10m",    // rotate if file hits 10MB
+            maxFiles: "14d",   // delete logs older than 14 days
+        }),
     ],
     format: winston.format.combine(
         winston.format.timestamp(),
